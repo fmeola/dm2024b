b650db2a05bd6abc86ef5839346d210b836c77f9
fmeola <fmeola@itba.edu.ar>
2024-06-23 14:02:53 +0000

    auto commit 1       20240623 140251

---
+++ src/ambiente/301_GrabarMisSemillas.r
@@ -0,0 +1,18 @@
+# Este script almacena definitivamente sus cinco semillas
+# en el bucket, de forma que NO deba cargarlas en cada script
+
+require( "data.table" )
+
+# reemplazar aqui por SUS semillas 
+mis_semillas <- c(479023,
+                  704783,
+                  349099,
+                  364627,
+                  628801)
+
+tabla_semillas <- as.data.table(list( semilla = mis_semillas ))
+
+fwrite( tabla_semillas,
+    file = "~/buckets/b1/datasets/mis_semillas.txt",
+    sep = "\t"
+)
