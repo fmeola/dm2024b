898421ebfcb902579548c44569f59e28b5ba1aab
fmeola <fmeola@itba.edu.ar>
2024-07-02 02:30:17 +0000

    Drift 2

--- src/wf-etapas/541_DR_corregir_drifting.r
+++ src/wf-etapas/541_DR_corregir_drifting.r
@@ -107,6 +107,52 @@ drift_ambito <- function(campos_monetarios) {
 }
 
 #------------------------------------------------------------------------------
+# Drift UVA
+# El valor del UVA del último día de cada mes
+# API: https://www.bcra.gob.ar/BCRAyVos/catalogo-de-APIs-banco-central.asp
+#   Unidad de Valor Adquisitivo (UVA) (en pesos -con dos decimales-, base 31.3.2016=14.05)
+#   Ejemplo: GET http://api.bcra.gob.ar/estadisticas/v2.0/datosvariable/31/2019-01-01/2019-01-01
+
+drift_uva <- function(campos_monetarios) {
+  cat( "inicio drift_uva()\n")
+  
+  vfoto_mes <- c(
+    201901, 201902, 201903, 201904, 201905, 201906,
+    201907, 201908, 201909, 201910, 201911, 201912,
+    202001, 202002, 202003, 202004, 202005, 202006,
+    202007, 202008, 202009, 202010, 202011, 202012,
+    202101, 202102, 202103, 202104, 202105, 202106,
+    202107, 202108, 202109
+  )
+  
+  vUVA <- c(
+    32.030000, 32.860000, 33.970000, #2019
+    35.420000, 36.890000, 38.030000,
+    39.200000, 40.160000, 41.260000,
+    43.430000, 45.420000, 47.160000, 
+    49.050000, 50.490000, 51.620000, #2020
+    52.950000, 54.240000, 55.060000,
+    56.090000, 57.170000, 58.520000,
+    60.160000, 61.940000, 64.320000,
+    66.540000, 69.040000, 71.920000, #2021
+    74.870000, 78.070000, 81.130000,
+    83.820000, 86.170000, 88.760000
+  )
+  
+  tb_UVA <- as.data.table( list( vfoto_mes, vUVA) )
+  
+  colnames( tb_UVA ) <- c( envg$PARAM$dataset_metadata$periodo, "UVA" )
+  
+  dataset[tb_UVA,
+          on = c(envg$PARAM$dataset_metadata$periodo),
+          (campos_monetarios) := .SD / i.UVA, # Divido cada monto por el precio del UVA
+          .SDcols = campos_monetarios
+  ]
+  
+  cat( "fin drift_uva()\n")
+}
+
+#------------------------------------------------------------------------------
 # deflaciona por IPC
 # momento 1.0  31-dic-2020 a las 23:59
 
@@ -238,7 +284,8 @@ switch(envg$PARAM$metodo,
   "deflacion"      = drift_deflacion(campos_monetarios),
   "estandarizar"   = drift_estandarizar(campos_monetarios),
   "bcra"           = drift_bcra(campos_monetarios),
-  "ambito"         = drift_ambito(campos_monetarios)
+  "ambito"         = drift_ambito(campos_monetarios),
+  "uva"            = drift_uva(campos_monetarios)
 )
 
 
