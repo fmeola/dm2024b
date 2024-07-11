765c8d32a86ab58a9854e495db6d5c5e1478d67d
fmeola <fmeola@itba.edu.ar>
2024-07-11 16:13:01 +0000

    Corrida TS 2

--- src/workflows/619_workflow_base_f202109.r
+++ src/workflows/619_workflow_base_f202109.r
@@ -269,17 +269,17 @@ TS_strategy_base9 <- function( pinputexps )
   #   202103, 202102, 202101, 202012, 202011)
   param_local$final_train <- c(202107, 202106, 202105, 202104,
                                202103, 202102, 202101, 202012,
-                               202011, 202002, 202001, 201912)
+                               202011)
   # param_local$train$training <- c(202105, 202104, 202103,
   #    202102, 202101, 202012, 202011, 202010, 202009)
-  param_local$train$training <- c(202102, 202101, 202012, 202011,
-                                  202002, 202001, 201912, 202911,
-                                  201910, 201909, 201908, 201907)
-  # param_local$train$validation <- c(202106)
-  param_local$train$validation <- c(202104)
+  param_local$train$training <- c(202105, 202104, 202103, 202102,
+                                  202101, 202012, 202011, 202010,
+                                  202009)
+  param_local$train$validation <- c(202106)
+  # param_local$train$validation <- c(202104)
   # param_local$train$testing <- c(202107)
   # Grupo A Mas de un mes de train testing da mejores resultados
-  param_local$train$testing <- c(202107, 202106, 202105)
+  param_local$train$testing <- c(202107, 202106)
   
   # Atencion  0.2  de  undersampling de la clase mayoritaria,  los CONTINUA
   # 1.0 significa NO undersampling
